ff21bab0492df7cba3cfd5bf80630ef8b3950411
kprahman <narmak.namhar@gmail.com>
2018-06-11 16:54:09 -0400

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-	def test_bad_math(self):
-		self.assertEqual(1 + 1, 3)
+	def test_root_url_resolves_to_home_page(self):
+		found = resolve('/') ##when we refer to this url (the root of the site)
+		self.assertEqual(found.func, home_page) ##we're looking for the home_page function
+		##because when we navigate to the root url, we want the home page to appear.
+
+
