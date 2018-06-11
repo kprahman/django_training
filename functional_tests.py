953d0cf1324111a09083cad740ce1156d892805d
kprahman <narmak.namhar@gmail.com>
2018-06-11 19:56:41 -0400

    Function test now checks that we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
@@ -15,15 +17,30 @@ class NewVisitorTest(unittest.TestCase):
 
 		##user sees that the title of the page mentions to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail("Finish the test!")
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
-		##user is invited to input a todo item 
+		##user is invited to input a todo item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			) 
 
 		##user enters the item into a text bok
+		inputbox.send_keys('Buy peacock feathers')
 
 		##user hits enter, the page updates, and lists "1:item"
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+			)
 
 		##text bok remains, allowing the user to go again
+		self.fail("Nailed it! Take a shot.")
 
 		##page updates again if the user enters another item
 
